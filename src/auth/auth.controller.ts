421ba1911ca0f20b7b3a940842e080e9b43ff35a
David <david.krtolica8@gmail.com>
2022-05-18 20:14:15 +0200

    Finished authentication using authGuards and JWT - still missing bcrypt password hashing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,40 +1,57 @@
-import { Controller, Get, Param, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Delete, Body, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Auth } from './auth.entity';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  //GET ALL AUTH OBJECTS 
+  //GET ALL AUTH OBJECTS
+  @UseGuards(JwtAuthGuard)
   @Get('auths')
   async getAllAuths(): Promise<Auth[]> {
     return await this.authService.findAll();
   }
 
   //GET ONE AUTH BY ID
+  @UseGuards(JwtAuthGuard)
   @Get('auths/:searchAuthId')
   async getAuthById(@Param('searchAuthId') searchAuthId: number): Promise<Auth> {
     return await this.authService.findOne(searchAuthId);
   }
 
   //DELETING AN EXISTING AUTH OBJECT BY ID
+  @UseGuards(JwtAuthGuard)
   @Delete('auths/:deleteId')
   async deleteAuthById(@Param('deleteId') deleteId: number): Promise<any> {
     return await this.authService.deleteById(deleteId);
   }
 
   //CREATING A COMPLETELY NEW AUTH OBJECT
+  @UseGuards(JwtAuthGuard)
   @Post('auths')
   async createAuth(@Body() authData: Auth): Promise<any> {
     return this.authService.create(authData);
   }
 
   //UPDATING AN ALREADY EXISTING AUTH
+  @UseGuards(JwtAuthGuard)
   @Put('auths/:updateId')
   async updateAuth(@Param('updateId') updateAuthId, @Body() authData: Auth): Promise<any> {
     authData.authId = Number(updateAuthId);
     console.log(`Update Auth @ ${authData.authId}`);
     return this.authService.updateById(authData);
+  }
+
+  //////////////////////////////////// AUTHENTICATION ////////////////////////////////////////////////
+
+  //LOGIN METHOD ENDPOINT
+  @UseGuards(LocalAuthGuard)
+  @Post('auths/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeleteResult, UpdateResult } from 'typeorm';
 import { Auth } from './auth.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(Auth)
     private authRepository: Repository<Auth>,
+    private jwtService: JwtService
   ) {}
 
   async findAll(): Promise<Auth[]> {
@@ -28,5 +30,23 @@ export class AuthService {
 
   async updateById(auth: Auth): Promise<UpdateResult> {
     return await this.authRepository.update(auth.authId, auth);
+  }
+
+  /////////////////////////////// AUTHENTICATION ///////////////////////////////////////////////////
+
+  async validateAuth(emailInput: string, passwordInput: string): Promise<any> {
+    const auth = await this.authRepository.findOne({ where: { email: emailInput } });
+    if (auth.password ===  passwordInput) {
+      return auth;
+    }
+    throw new NotFoundException();
+  }
+
+  async login(auth: any) {
+    console.log(auth);
+    const payload = { email: auth.email, sub: auth.authId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
