6df17557591eed86652b8075f650fc059702928e
David <david.krtolica8@gmail.com>
2022-05-24 20:47:51 +0200

    Added Create, Update and Delete for Estate - MongoDB

--- src/estate/estate.controller.ts
+++ src/estate/estate.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Estate } from './estate.schema';
 import { EstateService } from './estate.service';
 
@@ -16,5 +17,29 @@ export class EstateController {
   @Get('estates/:searchEstateId')
   async getEstateById(@Param('searchEstateId') searchEstateId: number): Promise<Estate> {
     return await this.estateService.findOne(searchEstateId);
+  }
+
+  //CREATE A NEW ESTATE POST METHOD
+  //PROTECTED - NEED TO PASS JWT TOKEN
+  @UseGuards(JwtAuthGuard)
+  @Post('estates')
+  async createNewEstate(@Body() estateInput: any): Promise<any> {
+    return await this.estateService.insertEstate(estateInput);
+  }
+
+  //DELETE ONE ESTATE BY ID
+  //PROTECTED - NEED TO PASS JWT TOKEN
+  @UseGuards(JwtAuthGuard)
+  @Delete('estates/:deleteEstateId')
+  async deleteEstateById(@Param('deleteEstateId') deleteEstateId: number): Promise<any> {
+    return await this.estateService.deleteAnEstateByEstateId(deleteEstateId);
+  }
+
+  //UPDATE AN EXISTING ESTATE - PUT METHOD
+  //PROTECTED - NEED TO PASS JWT TOKEN
+  @UseGuards(JwtAuthGuard)
+  @Put('estates/:updateEstateId')
+  async updateEstateById(@Param('updateEstateId') updateEstateId: number, @Body() updatedEstateInput: any): Promise<any> {
+    return await this.estateService.updateEstateById(updateEstateId, updatedEstateInput);
   }
 }
--- src/estate/estate.service.ts
+++ src/estate/estate.service.ts
@@ -14,4 +14,58 @@ export class EstateService {
   async findOne(searchEstateId: number): Promise<Estate> {
       return this.estateModel.findOne({ estateId: searchEstateId }).exec();
   }
+
+  async insertEstate(estate: any) {
+    const newEstate = new this.estateModel({
+      estateId: estate.estateId,
+      city: estate.city,
+      estateAddress: estate.estateAddress,
+      estateType: estate.estateType,
+      estateSize: estate.estateSize,
+      rooms: estate.rooms,
+      ownerId: estate.ownerId,
+      targetPrice: estate.targetPrice,
+      forRent: estate.forRent,
+      forSale: estate.forSale
+    });
+    const result = await newEstate.save();
+    return result;
+  }
+
+  async deleteAnEstateByEstateId(deleteEstateId: number): Promise<any> {
+    return this.estateModel.deleteOne({ estateId: deleteEstateId }).exec();
+  }
+
+  async updateEstateById(searchId: number, updatedEstate: any) {
+    const estate = await this.estateModel.findOne({ estateId: searchId }).exec();
+    if (updatedEstate.city) {
+      estate.city = updatedEstate.city;
+    }
+    if (updatedEstate.estateAddress) {
+      estate.estateAddress = updatedEstate.estateAddress;
+    }
+    if (updatedEstate.estateType) {
+      estate.estateType = updatedEstate.estateType;
+    }
+    if (updatedEstate.estateSize) {
+      estate.estateSize = updatedEstate.estateSize;
+    }
+    if (updatedEstate.rooms) {
+      estate.rooms = updatedEstate.rooms;
+    }
+    if (updatedEstate.ownerId) {
+      estate.ownerId = updatedEstate.ownerId;
+    }
+    if (updatedEstate.targetPrice) {
+      estate.targetPrice = updatedEstate.targetPrice;
+    }
+    if (updatedEstate.forRent) {
+      estate.forRent = updatedEstate.forRent;
+    }
+    if (updatedEstate.forSale) {
+      estate.forSale = updatedEstate.forSale;
+    }
+    estate.save();
+    return `Successfully updated Estate with ID: ${estate.estateId} at address ${estate.estateAddress} !`
+  }
 }
